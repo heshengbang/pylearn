15efcf4d08e150baeb67bf07bbbadecbd253a20d
heshengbang <yunshixin8521894@sohu.com>
2017-12-08 19:04:23 +0800

    add files

--- web/hello.py
+++ web/hello.py
@@ -6,10 +6,11 @@ print                               # 空行，告诉服务器结束头部
 print '<html>'
 print '<head>'
 print '<meta charset="utf-8">'
-print '<title>Hello Word - 我的第一个 CGI 程序！</title>'
+print '<title>我的第一个 CGI 程序！</title>'
 print '</head>'
 print '<body>'
 print '<h2>Hello Word! 我是来自菜鸟教程的第一CGI程序</h2>'
+print '<div>总是需要重启服务器吗？测试一下就知道类</div>'
 print '</body>'
 print '</html>'
 #http://blog.csdn.net/nicholas_wong/article/details/70226246
---
+++ web/hello2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+print "Content-type:text/html"
+print
+print '<html>'
+print '<head>'
+print '<meta charset="utf-8">'
+print '<title>Hello World</title>'
+print '</head>'
+print '<body>'
+print 'Hello World'
+print '</body>'
+print '</html>'
